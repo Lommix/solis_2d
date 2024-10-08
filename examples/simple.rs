4cb0850fc4c840d2cf5c782f41643b269f23f2b2
Lorenz Mielke <info@lommix.de>
2024-10-08 15:10:33 +0200

    new example

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,8 +1,6 @@
 use bevy::{
-    core_pipeline::tonemapping::Tonemapping,
-    diagnostic::{DiagnosticsStore, FrameTimeDiagnosticsPlugin},
-    input::mouse::MouseWheel,
-    prelude::*,
+    core_pipeline::tonemapping::Tonemapping, diagnostic::FrameTimeDiagnosticsPlugin,
+    input::mouse::MouseWheel, prelude::*, render::primitives::Aabb,
 };
 use bevy_egui::*;
 use lommix_light::prelude::*;
@@ -24,38 +22,15 @@ pub fn main() {
         .add_systems(Startup, (setup, spawn_info_box))
         .add_systems(
             Update,
-            (
-                update,
-                scroll,
-                move_camera,
-                move_light,
-                spawn_light,
-                clear,
-                config,
-                diagnostics,
-                monitor,
-            ),
+            (scroll, move_camera, move_light, spawn_light, clear, config),
         )
         .run();
 }
 
 #[derive(Component)]
-struct Spin(f32);
-
-#[derive(Component)]
 struct FollowMouse;
 
-fn monitor(diagnostics: Res<DiagnosticsStore>) {
-    let Some(_fps) = diagnostics
-        .get(&bevy::diagnostic::FrameTimeDiagnosticsPlugin::FRAME_TIME)
-        .map(|fps| fps.value())
-        .flatten()
-    else {
-        return;
-    };
-}
-
-fn setup(mut cmd: Commands, server: Res<AssetServer>) {
+fn setup(mut cmd: Commands) {
     cmd.spawn(RadianceCameraBundle {
         camera_bundle: Camera2dBundle {
             transform: Transform::from_translation(Vec3::new(0.0, 0.0, 5.0))
@@ -71,45 +46,24 @@ fn setup(mut cmd: Commands, server: Res<AssetServer>) {
         ..default()
     });
 
-    for x in -4..=8 {
-        for y in -4..=8 {
-            cmd.spawn((
-                SpriteBundle {
-                    texture: server.load("box.png"),
-                    transform: Transform::from_xyz((x as f32) * 400., (y as f32) * 400., 1.),
-                    ..default()
-                },
-                Emitter {
-                    intensity: 1.,
-                    color: Color::BLACK,
-                    shape: SdfShape::Rect(Vec2::new(50., 25.)),
-                },
-                Spin(rand::random::<f32>()),
-            ));
-        }
-    }
-    cmd.spawn((SpriteBundle {
-        sprite: Sprite {
-            custom_size: Some(Vec2::splat(2000.)),
-            ..default()
+    cmd.spawn((
+        SpriteBundle::default(),
+        Aabb::default(),
+        Emitter {
+            intensity: 1.,
+            color: Color::BLACK,
+            shape: SdfShape::Rect(Vec2::new(50., 25.)),
         },
-        texture: server.load("box.png"),
-        transform: Transform::from_translation(Vec3::new(0., 0., 0.)),
-        ..default()
-    },));
-
+    ));
     cmd.spawn((
         Emitter {
             intensity: 1.,
             color: Color::WHITE,
             shape: SdfShape::Circle(200.),
         },
+        Aabb::default(),
         FollowMouse,
-        SpriteBundle {
-            texture: server.load("lamp.png"),
-            transform: Transform::from_translation(Vec3::new(0., 0., 0.)),
-            ..default()
-        },
+        SpriteBundle::default(),
     ));
 }
 
@@ -137,49 +91,6 @@ fn config(mut gi_config: Query<(&mut RadianceConfig, &mut RadianceDebug)>, mut e
         });
 }
 
-#[derive(Default)]
-struct Timings {
-    count: u32,
-    acc_fps: f64,
-    acc_frame: f64,
-    avg_fps: f64,
-    avg_frame: f64,
-}
-
-fn diagnostics(
-    mut egui: EguiContexts,
-    diagnostics: Res<DiagnosticsStore>,
-    mut timings: Local<Timings>,
-) {
-    let Some(fps_time) = diagnostics.get_measurement(&FrameTimeDiagnosticsPlugin::FPS) else {
-        return;
-    };
-
-    let Some(frame_time) = diagnostics.get_measurement(&FrameTimeDiagnosticsPlugin::FRAME_TIME)
-    else {
-        return;
-    };
-
-    timings.acc_fps += fps_time.value;
-    timings.acc_frame += frame_time.value;
-    timings.count += 1;
-
-    if timings.count == 100 {
-        timings.avg_fps = timings.acc_fps * 0.01;
-        timings.avg_frame = timings.acc_frame * 0.01;
-        timings.count = 0;
-        timings.acc_fps = 0.;
-        timings.acc_frame = 0.;
-    }
-
-    egui::Window::new("Diagnostics")
-        .anchor(egui::Align2::RIGHT_BOTTOM, [0., 0.])
-        .show(egui.ctx_mut(), |ui| {
-            ui.label(format!("FPS: {:.3}", timings.avg_fps));
-            ui.label(format!("FRAME: {:.2}ms", timings.avg_frame));
-        });
-}
-
 fn flag_checkbox(bit: GiFlags, ui: &mut egui::Ui, flags: &mut GiFlags, label: &str) {
     let mut state = (*flags & bit) != GiFlags::DEFAULT;
     ui.checkbox(&mut state, label);
@@ -188,38 +99,6 @@ fn flag_checkbox(bit: GiFlags, ui: &mut egui::Ui, flags: &mut GiFlags, label: &s
     } else {
         *flags &= !bit;
     }
-}
-
-fn update(mut query: Query<(&mut Transform, &Spin)>, time: Res<Time>) {
-    query.iter_mut().for_each(|(mut transform, spin)| {
-        transform.rotation = Quat::from_rotation_z(time.elapsed_seconds() * spin.0);
-    });
-}
-
-fn spawn_info_box(mut cmd: Commands) {
-    let mut node = NodeBundle::default();
-    node.style.width = Val::Percent(100.);
-    node.style.height = Val::Percent(100.);
-    node.style.align_items = AlignItems::End;
-    node.style.justify_content = JustifyContent::Start;
-
-    cmd.spawn(node).with_children(|cmd| {
-        let mut node = NodeBundle::default();
-        node.style.border = UiRect::all(Val::Px(4.));
-        node.background_color = BackgroundColor(Color::BLACK);
-        node.border_radius = BorderRadius::all(Val::Px(15.));
-        node.style.padding = UiRect::all(Val::Px(10.));
-        cmd.spawn(node).with_children(|cmd| {
-            cmd.spawn(TextBundle::from_section(
-                "[Arrowkeys]:Move [I]:Zoom-in [O]:Zoom-out [Wheel]:Inc-size [Wheel+shift]:Inc-intensity [R]: clear screen",
-                TextStyle {
-                    color: Color::WHITE,
-                    font_size: 20.,
-                    ..default()
-                },
-            ));
-        });
-    });
 }
 
 fn spawn_light(
@@ -274,7 +153,7 @@ fn spawn_light(
 
 fn clear(
     mut cmd: Commands,
-    emitters: Query<Entity, (Without<FollowMouse>, Without<Spin>, With<Emitter>)>,
+    emitters: Query<Entity, (Without<FollowMouse>, With<Emitter>)>,
     inputs: Res<ButtonInput<KeyCode>>,
 ) {
     if inputs.just_pressed(KeyCode::KeyR) {
@@ -364,4 +243,30 @@ fn move_light(
 
     light_transform.translation.x = cursor_pos.x / scale;
     light_transform.translation.y = cursor_pos.y / scale;
+}
+
+fn spawn_info_box(mut cmd: Commands) {
+    let mut node = NodeBundle::default();
+    node.style.width = Val::Percent(100.);
+    node.style.height = Val::Percent(100.);
+    node.style.align_items = AlignItems::End;
+    node.style.justify_content = JustifyContent::Start;
+
+    cmd.spawn(node).with_children(|cmd| {
+        let mut node = NodeBundle::default();
+        node.style.border = UiRect::all(Val::Px(4.));
+        node.background_color = BackgroundColor(Color::BLACK);
+        node.border_radius = BorderRadius::all(Val::Px(15.));
+        node.style.padding = UiRect::all(Val::Px(10.));
+        cmd.spawn(node).with_children(|cmd| {
+            cmd.spawn(TextBundle::from_section(
+                "[Arrowkeys]:Move [I]:Zoom-in [O]:Zoom-out [Wheel]:Inc-size [Wheel+shift]:Inc-intensity [R]: clear screen",
+                TextStyle {
+                    color: Color::WHITE,
+                    font_size: 20.,
+                    ..default()
+                },
+            ));
+        });
+    });
 }
